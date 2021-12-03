a69ae36c01dd1b95e0b800c4ce106b42b8ce7708
abelzach <abelsimonz@gmail.com>
2021-12-03 21:43:54 +0530

    Intial Commit

---
+++ truffle-config.js
@@ -0,0 +1,39 @@
+require('babel-register');
+require('babel-polyfill');
+const fs = require('fs');
+
+const Kit = require('@celo/contractkit')
+const kit = Kit.newKit('https://alfajores-forno.celo-testnet.org')
+const getAccount = require('./getAccount').getAccount
+
+async function awaitWrapper() {
+  let account = await getAccount()
+  kit.connection.addAccount(account.privateKey)
+}
+awaitWrapper() 
+
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    alfajores: {
+      provider: kit.connection.web3.currentProvider, 
+      network_id: 44787
+    }
+  },
+
+  contracts_build_directory: './src/build/',
+  compilers: {
+    solc: {
+      version: "0.8.2",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
